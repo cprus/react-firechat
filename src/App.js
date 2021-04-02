e6d776d346b39ccb8dc501c0024ca9d416be1ab0
Collin P <collinprusse@mac.com>
2021-04-02 17:58:02 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+import Button from './components/Button';
+import Channel from './components/Channel';
+
+const { 
+  REACT_APP_API_KEY, 
+  REACT_APP_AUTH_DOMAIN, 
+  REACT_APP_PROJ_ID, 
+  REACT_APP_STOR_BUCKET, 
+  REACT_APP_SENDER_ID,
+  REACT_APP_APP_ID
+} = process.env;
+
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: REACT_APP_API_KEY,
+    authDomain: REACT_APP_AUTH_DOMAIN,
+    projectId: REACT_APP_PROJ_ID,
+    storageBucket: REACT_APP_STOR_BUCKET,
+    messagingSenderId: REACT_APP_SENDER_ID,
+    appId: REACT_APP_APP_ID
+});
+}
+
+
+const auth = firebase.auth();
+const db = firebase.firestore();
 
 function App() {
+  const [user, setUser] = useState(() => auth.currentUser);
+  const [initializing, setInitializing] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+      if (initializing) {
+        setInitializing(false);
+      }
+    });
+
+    // Cleanup subscription
+    return unsubscribe;
+  }, []);
+
+  const signInWithGoogle = async () => {
+    //retrieve google provider object
+    const provider = new firebase.auth.GoogleAuthProvider();
+    // Set language to the default browser preference
+    auth.useDeviceLanguage();
+    // Start sign in process
+    try {
+      await auth.signInWithPopup(provider);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const signOut = async () => {
+    try {
+      await firebase.auth().signOut();
+    } catch (error) {
+      console.log(error.message)
+    }
+  };
+
+  if (initializing) return 'Loading...';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {user ? (
+        <>
+          <Button onClick={signOut}>Sign Out</Button>
+          <Channel user={user} db={db}/>
+        </>
+        
+      ) : (
+        <Button onClick={signInWithGoogle}>Sign In with Google</Button>
+      )}
     </div>
   );
 }
